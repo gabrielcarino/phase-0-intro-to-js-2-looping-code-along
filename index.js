05b17ac09fd833a864a5e68d6f9a2a0632db1de6
Gabriel Carino <gabejcarino@gmail.com>
2021-12-07 14:08:27 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const Cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(Cards, event) {
+    let messages = [];
+    for (let i = 0; i < Cards.length; i++) {
+        messages.push(`Thank you, ${Cards[i]}, for the wonderful surprise gift!`)
+    }
+    return messages
+}
+function countDown() {
+    for (let countdown = 10; countdown > -1; countdown --) {
+        console.log(countdown);
+    }
+}
